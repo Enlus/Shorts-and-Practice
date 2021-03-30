fdfc05d17528ed7339153737a458a2896847c1d9
Enlus <81607571+Enlus@users.noreply.github.com>
2021-03-30 01:07:31 -0400

    Add files via upload

---
+++ .github/workflows/Find Volume.py
@@ -0,0 +1,9 @@
+height_str = input("What is your height?")
+width_str = input("What is your width?")
+length_str = input("What is your length?")
+
+height_int = int(height_str)
+width_int = int(width_str)
+length_int = int(length_str)
+volume_int = height_int*width_int*length_int
+print(volume_int)
